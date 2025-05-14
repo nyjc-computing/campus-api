c15e6a386ffb4594c6adc259b99c3b7c6c09c8fd
JS Ng <ngjunsiang@gmail.com>
2025-05-15 06:18:21 +0800

    Update components.py imports

--- openapi/components.py
+++ openapi/components.py
@@ -3,16 +3,22 @@
 Classes for representing the components section of OpenAPI.
 """
 
+from .datatypes import Schema
+from .path import Parameter
+from .request import RequestBody
+from .response import Response
+
+
 class Components:
     """Represents a collection of components as defined in OpenAPI 3.0.
 
     https://swagger.io/docs/specification/v3_0/components/
     """
-    schemas: list
-    parameters: list
+    schemas: list[Schema]
+    parameters: list[Parameter]
     securitySchemes: list
-    requestBodies: list
-    responses: list
+    requestBodies: list[RequestBody]
+    responses: list[Response]
     headers: list
     examples: list
     links: list
