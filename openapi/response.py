bffb1c0aaa626764affeb7b37ac402c6edc747fc
JS Ng <ngjunsiang@gmail.com>
2025-05-14 23:37:00 +0800

    move Response schema to response.py

---
+++ openapi/response.py
@@ -0,0 +1,29 @@
+"""openapi/paths
+
+Classes for representing API paths
+"""
+
+from typing import Mapping
+
+Mimetype: str
+Content: Mapping[Mimetype, Schema]
+
+
+class Response:
+    """Represents a response as defined in OpenAPI 3.0.
+
+    https://swagger.io/docs/specification/v3.0/describing-responses/
+    """
+    status_code: str  # OpenAPI represents status codes as strings
+    description: str | None
+    content: Content
+
+    def __init__(
+            self,
+            status_code: str
+            content: Content
+            description: str | None = None,
+    ):
+        self.status_code = status_code
+        self.description = description
+        self.content = dict(content)
