d3c6d0b5ee603429c798cfae08698693c1f28520
JS Ng <ngjunsiang@gmail.com>
2025-05-15 15:29:21 +0000

    refactor: move get_client into api

--- campus/__init__.py
+++ campus/__init__.py
@@ -1,15 +1,9 @@
 """campus - A Python wrapper for the Campus API."""
 
-from .api import CampusClient
-from . import config
+from .api import CampusClient, get_client
 
-
-def get_client(cfg: dict | None = None, **kwargs) -> CampusClient:
-    """Get a CampusClient instance."""
-    cfg = cfg or config.default.copy()
-    cfg.update(kwargs)
-    return CampusClient.from_config(cfg)
 
 __all__ = [
     "CampusClient",
+    "get_client",
 ]
--- campus/api/__init__.py
+++ campus/api/__init__.py
@@ -8,7 +8,15 @@ represented as methods.
 """
 
 from campus.api.api import CampusClient
+from campus import config
 
 __all__ = [
     "CampusClient",
 ]
+
+
+def get_client(cfg: dict | None = None, **kwargs) -> CampusClient:
+    """Get a CampusClient instance."""
+    cfg = cfg or config.default.copy()
+    cfg.update(kwargs)
+    return CampusClient.from_config(cfg)
