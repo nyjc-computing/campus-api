5f32253e447703128bd5608d6c609c95b7db9189
JS Ng <ngjunsiang@gmail.com>
2025-05-15 03:17:50 +0000

    support hidden, request-only, and response-only fields

--- campus/modeltypes.py
+++ campus/modeltypes.py
@@ -22,6 +22,12 @@ class CampusModel(dict[str, Validatable]):
     This class is used to represent a model in the campus system.
     Models subclass Python dict for easier compatibility with JSON.
     """
+    # Hidden properties are not specified in requests, and not returned in responses
+    __hidden__: tuple[str] = ()
+    # Request-only properties are specified in requests, but not returned in responses
+    __request_only__: tuple[str] = ()
+    # Response-only properties are not specified in requests, but returned in responses
+    __response_only__: tuple[str] = ()
     # Models must declare all properties as class variables.
     # Properties must use the Validatable type.
 
@@ -32,10 +38,19 @@ class CampusModel(dict[str, Validatable]):
         This init hook is used to validate properties and provide a validators
         class variable.
         """
-        cls.validators = dict(cls.__annotations__.items())
+        cls.validators = {}
         for field, typecls in cls.validators.items():
             if not isinstance(typecls, Validatable):
                 raise TypeError(f"Field {field} must be of type Validatable")
+            if field in cls.__hidden__ and field in cls.__request_only__:
+                raise TypeError(
+                    f"Field {field} cannot be both hidden and request-only"
+                )
+            if field in cls.__hidden__ and field in cls.__response_only__:
+                raise TypeError(
+                    f"Field {field} cannot be both hidden and response-only"
+                )
+            cls.validators[field] = typecls
 
     def __init__(self, **kwargs):
         """__init__ is called when an instance is created.
@@ -44,12 +59,25 @@ class CampusModel(dict[str, Validatable]):
         the given keyword arguments.
         """
         invalid_fields = set(kwargs) - set(self.validators)
-        missing_fields = set(self.validators) - set(kwargs)
+        missing_fields = (
+            set(self.validators)
+            - set(kwargs)
+            - set(self.__hidden__)
+            - set(self.__response_only__)
+        )
         if invalid_fields:
             raise KeyError(f"Invalid fields: {invalid_fields}")
         if missing_fields:
             raise KeyError(f"Missing fields: {missing_fields}")
         for field, Typecls in self.validators:
+            if field in self.__hidden__:
+                raise KeyError(
+                    f"Field {field} is hidden and should not be passed in init"
+                )
+            if field in self.__response_only__:
+                raise KeyError(
+                    f"Field {field} is response-only and should not be passed in init"
+                )
             assert isinstance(Typecls, Validatable)  # appease linter
             Typecls.validate(kwargs[field])
         # Cast all values to their respective Validatable types
@@ -76,8 +104,10 @@ class User(CampusModel):
 
     This model is used to represent a user in the campus system.
     """
+    __response_only__ = ("id", "activated_at")
     id: UserID
-    email: EmailAddress    
+    email: EmailAddress
+    activated_at: Datetime
 
 
 class Client(CampusModel):
@@ -85,6 +115,8 @@ class Client(CampusModel):
 
     This model is used to represent a client in the campus system.
     """
+    __hidden__ = ("secret_hash",)
+    __response_only__ = ("id", "created_at")
     id: CampusID
     name: CampusLabel
     description: String
