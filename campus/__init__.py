86809a836b03ff1feb3dfb8582f130f163831bc3
JS Ng <ngjunsiang@gmail.com>
2025-05-15 06:49:55 +0000

    add factory function

---
+++ campus/__init__.py
@@ -0,0 +1,14 @@
+from .wrapper import CampusClient
+
+from .utils import load_config
+
+default_config = load_config("config.json")
+
+def get_client(config: dict | None = default_config, **kwargs) -> CampusClient:
+    """Get a CampusClient instance."""
+    config = config.copy().update(kwargs)
+    return CampusClient.from_config(config)
+
+__all__ = [
+    "CampusClient",
+]
