874717f24ae7bcf25287c48d00a97a8b128c68ce
JS Ng <ngjunsiang@gmail.com>
2025-05-15 05:28:30 +0000

    refactor: use type expression string

--- campus/wrapper/base.py
+++ campus/wrapper/base.py
@@ -4,13 +4,14 @@ from campus.schema.datatypes import UserID, CampusID
 class CampusAPI:
     """A typing stub for the Campus API Client."""
 
+
 class CampusResource:
     """Base class for resources in the Campus API.
 
     This class provides a common interface for all resources.
     """
 
-    def __init__(self, parent: "CampusResource" | CampusAPI):
+    def __init__(self, parent: "CampusAPI | CampusResource"):
         self.parent = parent
 
     def __repr__(self):
