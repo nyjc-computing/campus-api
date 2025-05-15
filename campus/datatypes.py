fae96a2be9853518c4f50cbbfc75549bd1c60898
JS Ng <ngjunsiang@gmail.com>
2025-05-15 02:03:51 +0000

    refactor: do not carry out validation on instantiation

--- campus/datatypes.py
+++ campus/datatypes.py
@@ -19,8 +19,8 @@ LowerLetterWord = r'[a-z]{2,15}'
 ResourceName = LowerLetterWord
 # Campus allows hyphenated lowercase words (up to 3 parts) as name labels
 CampusLabel = fr'{LowerLetterWord}(-{LowerLetterWord}){{0,2}}'
-UserID = r'[a-zA-Z0-9._-]{1,64}'
-Domain = r'[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+UserIDPattern = r'[a-zA-Z0-9._-]{1,64}'
+DomainPattern = r'[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 EmailAddress = fr'{UserID}@{Domain}'
 
 Uid8Pattern = fr'{LowerLetterDecimalChar}{{8}}'
@@ -37,9 +37,43 @@ class StringPattern(str):
     pattern: re.Pattern
 
     def __new__(cls, value):
-        if not cls.pattern.match(value):
-            raise ValueError(f"Invalid string: {value}")
+        """Validation is not carried out at instantiation time."""
         return super().__new__(cls, value)
+    
+    @classmethod
+    def validate(cls, value: str) -> None:
+        """Validate the string value against the pattern.
+        
+        This method is defined as a class method, so it can be called
+        within __new__ if necessary.
+        """
+        if not cls.pattern.match(value):
+            raise ValueError(
+                f"Value does not match pattern {cls.pattern.pattern!r}: {value}"
+            )
+
+
+class UserID(StringPattern):
+    """User IDs are the username part of the email address, for simplicity."""
+    pattern = fr'^{UserIDPattern}$'
+
+
+class Domain(StringPattern):
+    """Domain names are the domain part of the email address."""
+    pattern = fr'^{DomainPattern}$'
+
+
+class EmailAddress(StringPattern):
+    """A typical email address."""
+    pattern = fr'^{UserIDPattern}@{DomainPattern}$'
+
+    def __new__(cls, value):
+        cls.validate(value)  # Ensure valid email address first
+        string = super().__new__(cls, value)
+        _user_id, _domain = value.split('@')
+        string.user_id = UserID(_user_id)
+        string.domain = Domain(_domain)
+        return string
 
 
 class UID(StringPattern):
@@ -53,11 +87,6 @@ class UID(StringPattern):
     """
     pattern: re.Pattern
 
-    def __new__(cls, value):
-        if not cls.pattern.match(value):
-            raise ValueError(f"Invalid UID: {value}")
-        return super().__new__(cls, value)
-
 
 class CampusID(UID):
     """Campus IDs are UIDs UidPrefixed with a namespace.
@@ -67,15 +96,21 @@ class CampusID(UID):
 
     Example: uid-client-12345678
     """
-    pattern = fr"^{UidPrefix}-{CampusName}-{Uid8Pattern}$"
+    pattern = fr"^{UidPrefix}-({CampusLabel})-({Uid8Pattern})$"
 
     def __new__(cls, value):
-        if not cls.pattern.match(value):
-            raise ValueError(f"Invalid UID: {value}")
-        return super().__new__(cls, value)
+        if not (match := cls.pattern.match(value)):
+            raise ValueError(
+                f"Value does not match pattern {cls.pattern.pattern!r}: {value}"
+            )
+        string = super().__new__(cls, value)
+        _label, _uid = match.groups()
+        string.label = CampusLabel(_label)
+        string.uid = UID(_uid)
+        return string
 
 
-class ClientID(UID):
+class ClientID(CampusID):
     """Client IDs are UIDs UidPrefixed with a namespace.
 
     The namespace is 'uid-client-' followed by a string of lowercase letters
@@ -84,14 +119,10 @@ class ClientID(UID):
     Example: uid-client-12345678
     """
     pattern = fr"^{UidPrefix}-client-{Uid8Pattern}$"
-
-    def __new__(cls, value):
-        if not cls.pattern.match(value):
-            raise ValueError(f"Invalid UID: {value}")
-        return super().__new__(cls, value)
+    label: Literal["client"]
 
 
-class CircleID(UID):
+class CircleID(CampusID):
     """Circle IDs are UIDs UidPrefixed with a namespace.
 
     The namespace is 'uid-circle-' followed by a string of lowercase letters
@@ -100,11 +131,7 @@ class CircleID(UID):
     Example: uid-circle-12345678
     """
     pattern = fr"^{UidPrefix}-circle-{Uid8Pattern}$"
-
-    def __new__(cls, value):
-        if not cls.pattern.match(value):
-            raise ValueError(f"Invalid UID: {value}")
-        return super().__new__(cls, value)
+    label: Literal["circle"]
 
 
 class OTP(StringPattern):
