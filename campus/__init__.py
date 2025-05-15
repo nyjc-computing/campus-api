d6e82279ee52b0cc1961211dc79bf5c0d21e75c4
JS Ng <ngjunsiang@gmail.com>
2025-05-15 15:05:12 +0000

    dev: rename

--- campus/__init__.py
+++ campus/__init__.py
@@ -1,13 +1,12 @@
 """campus_api_python - A Python wrapper for the Campus API."""
 
-from .wrapper import CampusClient
+from .api import CampusClient
 from . import config
 
 
 def get_client(cfg: dict | None = None, **kwargs) -> CampusClient:
     """Get a CampusClient instance."""
     cfg = cfg or config.default.copy()
-    assert cfg  # appease linter
     cfg.update(kwargs)
     return CampusClient.from_config(cfg)
 
