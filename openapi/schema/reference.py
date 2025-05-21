d7ea79576a4297393f696c611789c5412d20f2d1
JS Ng <ngjunsiang@gmail.com>
2025-05-21 12:17:30 +0000

    fix: SchemaReference inherits Schema ABC

--- openapi/schema/reference.py
+++ openapi/schema/reference.py
@@ -5,19 +5,24 @@ Classes for representing References
 
 from typing import Protocol
 
+from openapi.schema.datatypes import Schema
+
 RefPattern = str
 
 
-class Reference(Protocol):
+class Reference(Schema):
     """Represents a reference as defined in OpenAPI 3.0.
 
     https://swagger.io/docs/specification/v3_0/using-ref/
     """
-    ref: RefPattern | None
+    ref: RefPattern
 
     def __init__(self, ref: RefPattern):
         # TODO: Validate RefPattern
         self.ref = ref
+
+    def to_json(self) -> dict:
+        return {"$ref": self.ref}
 
 
 class PathReference(Reference):
